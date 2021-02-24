75f7105ece6068dbf5d98fb1e9cccb46e0945f79
Sara Riggs <skriggs72@yahoo.com>
2021-02-23 18:18:49 -0600

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # plotly_homework
+In this assignment, I've built an interactive dashboard to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+Use the D3 library to read in samples.json.
+
+Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in selected test ID.
+
+Create a bubble chart that displays each sample.
+
+Display the sample metadata, i.e., an individual's demographic information.
+
+Create the Gauge Chart to plot the weekly washing frequency of the individual.
+
+Update all of the plots any time that a new sample is selected.
+
+Deploy the app to GitHub Pages.
--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,22 @@
-function getPlots(id) {
+function getPlots(sample_id) {
 	//Read samples.json
 		d3.json("./static/data/samples.json").then (sampledata =>{
 			console.log(sampledata)
-			var ids = sampledata.samples[0].otu_ids;
+			var result_ids = sampledata.samples.filter(row => row.id === sample_id)[0]
+			console.log(result_ids.sample_values+"This is result ids")
+			var ids = result_ids.otu_ids;
 			console.log(ids)
-			var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+			var sampleValues =  result_ids.sample_values.slice(0,10).reverse();
 			console.log(sampleValues)
-			var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+			var labels =  result_ids.otu_labels.slice(0,10);
 			console.log (labels)
 		// get only top 10 otu ids for the plot OTU and reversing it. 
-			var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+			var OTU_top = ( result_ids.otu_ids.slice(0, 10)).reverse();
 		// get the otu id's to the desired form for the plot
 			var OTU_id = OTU_top.map(d => "OTU " + d);
 			console.log(`OTU IDS: ${OTU_id}`)
 		 // get the top 10 labels for the plot
-			var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+			var labels =  result_ids.otu_labels.slice(0,10);
 			console.log(`OTU_labels: ${labels}`)
 			var trace = {
 				x: sampleValues,
@@ -46,14 +48,14 @@ function getPlots(id) {
 		Plotly.newPlot("bar", data, layout);
 			// The bubble chart
 			var trace1 = {
-				x: sampledata.samples[0].otu_ids,
-				y: sampledata.samples[0].sample_values,
+				x: result_ids.otu_ids,
+				y: result_ids.sample_values,
 				mode: "markers",
 				marker: {
-					size: sampledata.samples[0].sample_values,
-					color: sampledata.samples[0].otu_ids
+					size: result_ids.sample_values,
+					color: result_ids.otu_ids
 				},
-				text:  sampledata.samples[0].otu_labels
+				text:  result_ids.otu_labels
 	
 			};
 	
@@ -119,6 +121,69 @@ function getPlots(id) {
 			getPlots(data.names[0]);
 			getDemoInfo(data.names[0]);
 		});
+
+		// Enter a speed between 0 and 180
+var level = 175;
+
+// Trig to calc meter point
+var degrees = 180 - level,
+	 radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+	 pathX = String(x),
+	 space = ' ',
+	 pathY = String(y),
+	 pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data = [{ type: 'scatter',
+   x: [0], y:[0],
+	marker: {size: 28, color:'850000'},
+	showlegend: false,
+	name: 'speed',
+	text: level,
+	hoverinfo: 'text+name'},
+  { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+  rotation: 90,
+  text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
+			'Slow', 'Super Slow', ''],
+  textinfo: 'text',
+  textposition:'inside',	  
+  marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+						 'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+						 'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+						 'rgba(255, 255, 255, 0)']},
+  labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: '<b>Gauge</b> <br> Speed 0-100',
+  height: 1000,
+  width: 1000,
+  xaxis: {zeroline:false, showticklabels:false,
+			 showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+			 showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('myDiv', data, layout, {showSendToCloud:true});
+
 	}
 
 	init();
